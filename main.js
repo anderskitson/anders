b9a33889fb0ce20c207b03f0a8e231beae0a666c
Anders Kitson <anders.kitson@canadadrives.ca>
2016-09-21 16:57:18 -0700

    swap click not working

--- main.js
+++ main.js
@@ -8,5 +8,31 @@ $( document ).ready(function() {
     });
     $("#menu").click(function(){
         $(".show").toggle();
+
+        $("h1").css("background-size","cover");
+        $("h1").css("height","400px");
+
+        // if ( $( this ).hasClass( "imgSwap" ) ) {
+        //     $("h1").css("background","url(img/diamonds.jpg");
+        //     $("#menu").removeClass("imgSwap");
+        //
+        // }
+
+
+    });
+
+    $(".imgSwap").click(function(){
+        // $("h1").css("background","url(img/anders.jpg");
+        $(this).addClass("imgShow");
+        $(this).removeClass("imgSwap");
+        console.log("test1");
+
+    });
+    $(".imgShow").click(function(){
+        // $("h1").css("background","url(img/diamonds.jpg");
+        $(this).addClass("imgSwap");
+        $(this).removeClass("imgShow");
+        console.log("test2");
+
     });
 });
